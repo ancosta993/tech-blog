9aa2fe0f0df824855770c750faf304c170f713bd
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-30 22:36:24 -0400

    added the User  model in models folder

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+// creat the User model
+class User extends Model{};
+
+// create the columns and fields for the user table 
+User.init(
+   {
+      id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         autoIncrement: true,
+         primaryKey: true
+      },
+      username:{
+         type: DataTypes.STRING,
+         allowNull: false,  
+      },
+      email: {
+         type: DataTypes.STRING,
+         allowNull: false,
+         unique: true,
+         validate: {
+            isEmail: true
+         }
+      },
+      password:{
+         type: DataTypes.STRING,
+         allowNull: false,
+         validate:{
+            len: [4]
+         }
+      }
+   },
+   {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+   }
+);
