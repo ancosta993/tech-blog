42570e9f1f6da516d8195c6ddd4b137849f050c4
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-30 23:44:43 -0400

    findOne method added for Post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -19,6 +19,31 @@ router.get('/', (req, res) => {
    });
 });
 
+// get one post
+router.get('/:id', (req, res) => {
+   Post.findOne({
+      where:{
+         id:req.params.id
+      },
+      attributes:['id', 'title','content','created_at'],
+      include: {
+         model:User,
+         attributes:['id','username']
+      }
+   })
+   .then(dbPostData => {
+      if(!dbPostData){
+         res.status(400).json({message: 'No Post with this id was found'});
+         return;
+      }
+      res.json(dbPostData);
+   })
+   .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+   });
+});
+
 // create posts using user id
 router.post('/', (req, res) => {
    Post.create(req.body)
