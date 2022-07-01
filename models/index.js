1d1f7c4cfe67b06553799ef1f2096cc432a91631
Anthony Dcosta <adcosta2000@gmail.com>
2022-07-01 00:10:56 -0400

    added association between comment and other models

--- models/index.js
+++ models/index.js
@@ -11,6 +11,18 @@ Post.belongsTo(User, {
    foreignKey: 'user_id'
 });
 
+// making association between comments, user, and post models
+Comment.belongsTo(User, {
+   foreignKey: 'user_id'
+});
+Comment.belongsTo(Post, {
+   foreignKey: 'post_id'
+});
+User.hasMany(Comment, {
+   foreignKey: 'user_id'
+});
+Post.hasMany(Comment, {
+   foreignKey: 'post_id'
+});
 
-
-module.exports = {User, Post}
+module.exports = {User, Post, Comment}
