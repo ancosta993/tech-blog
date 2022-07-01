fe9efdd6297debe9d97e2ba3bf59743609460a32
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-30 23:35:40 -0400

    added the findAll post method and included the username from user model, made association between user and post models

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,11 +1,29 @@
 const router = require('express').Router();
 const {User, Post} = require('../../models');
 
+// get all posts, including the user that submitted them
+router.get('/', (req, res) => {
+   Post.findAll({
+      attributes:['id', 'title','content','created_at'],
+      include:{
+         model:User,
+         attributes:['id', 'username']
+      }
+   })
+   .then(dbPostData => {
+      res.json(dbPostData);
+   })
+   .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+   });
+});
 
+// create posts using user id
 router.post('/', (req, res) => {
    Post.create(req.body)
-   .then(dbUserData => {
-      res.json(dbUserData)
+   .then(dbPostData => {
+      res.json(dbPostData)
    })
    .catch(err => {
       console.log(err);
--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// making connection between User and Post model
+User.hasMany(Post, {
+   foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+   foreignKey: 'user_id'
+});
+
 module.exports = {User, Post}
