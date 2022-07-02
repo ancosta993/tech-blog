3bc96373836b6a41c927be68aa54f040f53989de
Anthony Dcosta <adcosta2000@gmail.com>
2022-07-01 23:47:09 -0400

    imported express-handlebars and set its instance as the view engine

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
 const routes = require('./controllers/');//import all the routes
-
+const exhbs = require('express-handlebars'); // import express handlebars and set it to exhbs
+const hbs = exhbs.create({}) // create an instance of exhbs and set it to hbs
 
 // declare necessary variables
 const app = express();
 PORT = process.env.PORT || 3001;
+
+// use middlewears to set the view engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // use express middlwears
 app.use(express.static(path.join(__dirname, 'path'))); // serve the public folder
