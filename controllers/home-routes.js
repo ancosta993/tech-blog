0d4201e8364a1614dd46b0f092718a9d9702bec2
Anthony Dcosta <adcosta2000@gmail.com>
2022-07-02 20:46:01 -0400

    users can now leave comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,7 +40,7 @@ router.get('/posts/:id', (req, res) => {
          },
          {
             model:Comment,
-            attributes:['comment_text'],
+            attributes:['comment_text', 'created_at'],
             include:{
                model:User,
                attributes:['username']
--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -7,7 +7,21 @@ async function commentFormHandler(event){
     ];
    // get the inputs
    const comment_text = document.querySelector("textarea[name='comment-text']").value.trim();
-   alert(post_id);
+   if(comment_text){
+      const response = await fetch('/api/comments', {
+         method:'POST',
+         body:JSON.stringify({
+            comment_text,
+            post_id
+         }),
+         headers:{'Content-Type':'application/json'}
+      })
+      if (response.ok){
+         document.location.reload();
+      } else {
+         alert(response.statusText);
+      }
+   }
 }
 
 document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
