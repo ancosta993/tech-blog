79cf5a71c9740598314c9f89c4808ed372a60673
Anthony Dcosta <adcosta2000@gmail.com>
2022-07-03 12:33:29 -0400

    helpers formats date and plural

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,9 @@
 module.exports = {
    format_date: date => {
-      return `${new Date(data).getMonth +1}/${new Date(data).getDate()}/${new Date(data).getFullYear()}`;
+      return `${new Date(date).getMonth() +1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
    },
    format_plural: (word, amount)=>{
-      if(amount !== 1){
+      if(amount > 1){
          return `${word}s`;
       } 
 
